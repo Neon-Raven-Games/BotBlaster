a23e3e711d1374db1430c68c5645bd002558ac27
maross3 <maross334@gmail.com>
2024-08-31 00:13:40 -0400

    element ui

---
+++ Assets/Scripts/UI/BlasterSelectUI.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[Serializable]
+public class BlasterUIElement
+{
+    public ElementHoverUI elementHoverUI;
+    public TextMeshProUGUI text;
+
+    public void SetElement(ElementFlag element)
+    {
+        elementHoverUI.elementFlag = element;
+        text.text = element.ToString();
+    }
+}
+public class BlasterSelectUI : MonoBehaviour
+{
+    [SerializeField] private List<BlasterUIElement> elements;
+
+    public void SetElements(ElementFlag mask)
+    {
+        var i = 0;
+        foreach (var flag in Enum.GetValues(typeof(ElementFlag)))
+        {
+            var element = (ElementFlag) flag;
+            if (mask.HasFlag(element)) continue;
+            if (i >= elements.Count)
+            {
+                Debug.LogError("Flags exceed element texts.");
+                break;
+            }
+            
+            var elementText = elements[i++];
+            elementText.SetElement(element);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ElementHoverUI.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ElementHoverUI : MonoBehaviour, IPointerEnterHandler
+{
+    [SerializeField] internal ElementFlag elementFlag;
+    [SerializeField] private HandCannon handCannon;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        handCannon.blasterElement = elementFlag;
+    }
+}
